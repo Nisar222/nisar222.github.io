c887b31629901fd3ee350c6dc14f144e42aa9e2d
Nisar Khan <nisarkhan@gmail.com>
2019-08-24 23:45:48 +0400

    Upload Site Files

---
+++ js/form.js
@@ -0,0 +1,8 @@
+<script src="lib/jquery.min.js"></script>
+<script type="text/javascript">var submitted=false;</script>
+<script type="text/javascript">
+$('#gform').on('submit', function(e) {
+  $('#gform *').fadeOut(2000);
+  $('#gform').prepend('Your submission has been processed...');
+  });
+</script>
